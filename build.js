e9de2cd15afec87c0db6af491232f81718bcb20b
Diego Mónaco <dfmonaco@gmail.com>
2015-09-09 16:21:43 -0300

    Add markdown processing

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
-var Metalsmith = require('metalsmith');
+var Metalsmith = require('metalsmith')
+    markdown = require('metalsmith-markdown');
 
 Metalsmith(__dirname)
   .source('./src')
+  .use(markdown())
   .destination('./build')
   .build(function(err) {
     if (err) throw err;
