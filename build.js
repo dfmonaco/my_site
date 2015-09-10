c4fc260f7fd55229eab7f917c9bae0a5673d6a5e
Diego Mónaco <dfmonaco@gmail.com>
2015-09-10 13:35:00 -0300

    Nunjucks templates

--- build.js
+++ build.js
@@ -1,10 +1,36 @@
-var Metalsmith = require('metalsmith')
-    markdown = require('metalsmith-markdown');
+var metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
 
-Metalsmith(__dirname)
+var consolidate = require('consolidate');
+var nunjucks = require('nunjucks');
+
+var config = require('./config');
+var metadata;
+var env = 'development';
+
+nunjucks.configure('./templates');
+consolidate.requires.nunjucks = nunjucks
+
+if (env == 'production') {
+  metadata = config.production;
+} else {
+  metadata = config.development;
+};
+
+metalsmith(__dirname)
+  .metadata(metadata)
   .source('./src')
-  .use(markdown())
   .destination('./build')
+  .use(markdown())
+  .use(layouts({
+    engine: 'nunjucks',
+    directory: 'templates'
+   }))
   .build(function(err) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    } else {
+      console.log('Build successful!');
+    }
   });
---
+++ src/index.md
@@ -0,0 +1,19 @@
+---
+title: Home
+name: home
+layout: home.html
+priority: 1
+linkTitle: Home
+permalink: false
+---
+
+<img class="avatar" src="/images/me.jpg" title="Diego Mónaco">
+
+<h3 class="collapse">Diego Mónaco</h3>
+
+<p class="leading">Software Engineer</p>
+
+I solve problems with code.
+
+<p><a href="/projects" class="btn">View Projects</a></p>
+
