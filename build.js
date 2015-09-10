59353f091727b0f12f461a4827f0851d60477fa8
Diego Mónaco <dfmonaco@gmail.com>
2015-09-10 16:42:08 -0300

    Adding posts

--- build.js
+++ build.js
@@ -1,24 +1,32 @@
+// Metalsmith
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var collections = require('metalsmith-collections');
 
+// Modules
 var consolidate = require('consolidate');
 var nunjucks = require('nunjucks');
+var dateFilter = require('nunjucks-date-filter');
 
+// Local
 var config = require('./config');
 var globalData;
 var env = 'development';
 
-nunjucks.configure('./templates');
-consolidate.requires.nunjucks = nunjucks
-
+// Global data
 if (env == 'production') {
   globalData = config.production;
 } else {
   globalData = config.development;
 };
 
+// Nunjucks configuration
+dateFilter.setDefaultFormat('MMMM D, YYYY');
+nunjucks.configure('./templates').addFilter('date', dateFilter);
+consolidate.requires.nunjucks = nunjucks;
+
+// Metalsmith pipeline
 metalsmith(__dirname)
   .metadata(globalData)
   .source('./src')
---
+++ src/posts/test.md
@@ -0,0 +1,49 @@
+---
+title: Test post
+layout: post.html
+date: 2015-07-28
+author: Diego Mónaco
+tags: foo bar
+---
+
+Global yada yada.
+
+```js
+// Global data
+if (env == 'production') {
+  globalData = config.production;
+} else {
+  globalData = config.development;
+};
+```
+Pipeline sarasa
+
+```js
+metalsmith(__dirname)
+  .metadata(globalData)
+  .source('./src')
+  .destination('./build')
+  .use(collections({
+    posts: {
+      pattern: 'posts/*.md',
+      sortBy: 'date',
+      reverse: true
+    },
+    pages: {
+      pattern: '*.md',
+      sortBy: 'priority'
+    }
+   }))
+  .use(markdown())
+  .use(layouts({
+    engine: 'nunjucks',
+    directory: 'templates'
+   }))
+  .build(function(err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Build successful!');
+    }
+  });
+```
