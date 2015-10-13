ab076206aaf79e8e9dcb2219ad3c3f7f9638ba4a
Diego Mónaco <dfmonaco@gmail.com>
2015-10-13 16:36:28 -0300

    Update metalsmith.js sot handle sass and serve site

--- metalsmith.js
+++ metalsmith.js
@@ -10,8 +10,9 @@ var feed = require('metalsmith-feed');
 var excerpts = require('metalsmith-better-excerpts');
 var pagination = require('metalsmith-pagination');
 var tags = require('metalsmith-tags');
-var writemetadata = require('metalsmith-writemetadata');
 var sitemap = require('metalsmith-sitemap');
+var sass = require('metalsmith-sass');
+var serve = require('metalsmith-serve');
 
 // Modules
 var consolidate = require('consolidate');
@@ -21,6 +22,7 @@ var highlight = require('highlight.js');
 
 // Local
 var config = require('./config');
+var env = process.env.NODE_ENV; 
 var globalData;
 
 // Nunjucks configuration
@@ -28,114 +30,108 @@ dateFilter.setDefaultFormat('MMMM D, YYYY');
 nunjucks.configure('./templates').addFilter('date', dateFilter);
 consolidate.requires.nunjucks = nunjucks;
 
-function build(production) {
+// function build(production) {
   // Global data
-  if (production) {
-    globalData = config.production;
-  } else {
-    globalData = config.development;
-  };
+if (env == 'production') {
+  globalData = config.production;
+} else {
+  globalData = config.development;
+};
 
-  // Metalsmith pipeline
-  metalsmith(__dirname)
-    .clean(false)
-    .metadata(globalData)
-    .source('./src')
-    .destination('./build')
-    .use(drafts())
-    .use(collections({
-      posts: {
-        pattern: 'posts/*.md',
-        sortBy: 'date',
-        reverse: true
-      },
-      pages: {
-        pattern: '*.md',
-        sortBy: 'priority'
-      }
-     }))
-    .use(markdown({
-      langPrefix: 'hljs ',
-      highlight: function (code, lang) {
-        if (lang) {
-          return highlight.highlight(lang, code).value;
-        } else {
-          return highlight.highlightAuto(code).value;
-        }
-      }
-    }))
-    .use(excerpts({
-      pruneLength: 160
-    }))
-    .use(
-      branch(function(filename, props, i) {
-        return props.collection[0] == 'posts';
-      }).use(permalinks({
-        pattern: 'blog/:title',
-        relative: false
-      }))
-    )
-    .use(
-      branch(function(filename, props, i) {
-        return props.collection[0] == 'pages';
-      }).use(permalinks({
-        pattern: ':title',
-        relative: false
-      }))
-    )
-    .use(pagination({
-      'collections.posts': {
-        perPage: 10,
-        layout: 'collection.html',
-        first: 'blog/index.html',
-        path: 'blog/:num/index.html'
-      }
-    }))
-    .use(tags({
-      handle: 'tags',
-      layout: 'tags.html',
-      path: 'tags/:tag/index.html',
-      pathPage: 'tags/:tag/:num/index.html',
-      perPage: 10,
+// Metalsmith pipeline
+var siteBuild = metalsmith(__dirname)
+  .metadata(globalData)
+  .source('./src')
+  .destination('./build')
+  .use(drafts())
+  .use(collections({
+    posts: {
+      pattern: 'posts/*.md',
       sortBy: 'date',
       reverse: true
-    }))
-    .use(layouts({
-      engine: 'nunjucks',
-      directory: 'templates'
-    }))
-    .use(writemetadata({
-      bufferencoding: 'utf8',
-      collections: {
-        posts: {
-          output: {
-            asObject: true,
-            path: 'blog/index.json',
-            metadata: {
-              "type": "list"
-            }
-          },
-          ignorekeys: ['history', 'stats', 'next', 'template', 'previous', 'collection', 'mode'],
-        }
-      }
-    }))
-    .use(sitemap({
-      hostname: globalData.site.url,
-      defaults: {
-        lastModified: Date.now()
-      },
-      root: {
-        lastModifed: Date.now()
-      }
-    }))
-    .use(feed({collection: 'posts'}))
-    .build(function(err) {
-      if (err) {
-        throw err;
+    },
+    pages: {
+      pattern: '*.md',
+      sortBy: 'priority'
+    }
+   }))
+  .use(markdown({
+    langPrefix: 'hljs ',
+    highlight: function (code, lang) {
+      if (lang) {
+        return highlight.highlight(lang, code).value;
       } else {
-        console.log('Build successful!');
+        return highlight.highlightAuto(code).value;
       }
-    });
-}
+    }
+  }))
+  .use(excerpts({
+    pruneLength: 160
+  }))
+  .use(
+    branch(function(filename, props, i) {
+      return props.collection[0] == 'posts';
+    }).use(permalinks({
+      pattern: 'blog/:title',
+      relative: false
+    }))
+  )
+  .use(
+    branch(function(filename, props, i) {
+      return props.collection[0] == 'pages';
+    }).use(permalinks({
+      pattern: ':title',
+      relative: false
+    }))
+  )
+  .use(pagination({
+    'collections.posts': {
+      perPage: 10,
+      layout: 'collection.html',
+      first: 'blog/index.html',
+      path: 'blog/:num/index.html'
+    }
+  }))
+  .use(tags({
+    handle: 'tags',
+    layout: 'tags.html',
+    path: 'tags/:tag/index.html',
+    pathPage: 'tags/:tag/:num/index.html',
+    perPage: 10,
+    sortBy: 'date',
+    reverse: true
+  }))
+  .use(layouts({
+    engine: 'nunjucks',
+    directory: 'templates'
+  }))
+  .use(sitemap({
+    hostname: globalData.site.url,
+    defaults: {
+      lastModified: Date.now()
+    },
+    root: {
+      lastModifed: Date.now()
+    }
+  }))
+  .use(feed({collection: 'posts'}))
+  .use(sass({
+    outputDir: function(originalPath) { 
+      return originalPath.replace("scss", "css");
+    }
+  }));
 
-module.exports = build;
+if (env != 'production') {
+  siteBuild = siteBuild.use(serve({
+    port: 8080,
+    verbose: true
+  }));
+};
+
+siteBuild.build(function(err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Build successful!');
+  }
+});
